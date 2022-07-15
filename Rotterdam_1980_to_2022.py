643ed86193275394be3ec1e2ce90b4c81761ba35
Ducky <107194777+Ducky9527@users.noreply.github.com>
2022-07-15 23:27:21 +0200

    Update Rotterdam_1980_to_2022.py

--- Rotterdam_1980_to_2022.py
+++ Rotterdam_1980_to_2022.py
@@ -19,17 +19,17 @@ df = df.drop(columns = ["STN", "TNH", "TXH"])
 # add a separate column for "year" 
 # because we will need this column for counting how many days in a year 
 # fulfill the criteria that 
-# (1) the avg temp of the day was above 25C 
+# (1) the avg temp of the day was above 25C, or 
 # (2) the highest temp of the day was above 30C
 df['Year'] = df['YYYYMMDD'].dt.year
 
 
 
-# filtering out the days that fulfill the two criteria
+# filtering out the days that fulfill either of the two criteria
 dff = df.loc[(df["TX"] > 300) | (df["TG"] > 250)]
 print(dff)
 
-# count how many days each year(from 2000 to 2022 today) fulfill the two criteria  
+# count how many days each year(from 2000 to 2022 today) fulfill either of the two criteria  
 dff_count = dff.groupby(["Year"])["Year"].count()
 print(dff_count)
 
