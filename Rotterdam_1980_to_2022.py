0ba1b12b191de144c45207066cd8478368bf5417
Ducky <107194777+Ducky9527@users.noreply.github.com>
2022-07-15 23:25:47 +0200

    Update Rotterdam_1980_to_2022.py

--- Rotterdam_1980_to_2022.py
+++ Rotterdam_1980_to_2022.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
-#import Rotterdam data & parse the dates
+# import Rotterdam data & parse the date
 
 
+# the data was downloaded from "https://daggegevens.knmi.nl/" 
 # STN         LON(east)   LAT(north)  ALT(m)      NAME
 # 344         4.447       51.962      -4.30       Rotterdam   
 # TG        : Daily mean temperature in (0.1 degrees Celsius)
@@ -10,25 +11,25 @@ import pandas as pd
 # TX        : Maximum temperature (in 0.1 degrees Celsius)
 # TNH = the hour when the temp was the lowest
 # TXH = the hour when the temp was the highest
-df = pd.read_csv('rotterdam_temparature/RD2000_2022.csv', parse_dates = ["YYYYMMDD"])
+df = pd.read_csv('rotterdam_temparature/RD1980_2022.csv', parse_dates = ["YYYYMMDD"])
 
-#clean up the dataset by removing unnecessary rows
+# clean up the dataset by removing unnecessary rows
 df = df.drop(columns = ["STN", "TNH", "TXH"])
 
-#add a separate column for "year" 
-#because we will need this column for counting how many days in a year 
-#fulfill the criteria that 
+# add a separate column for "year" 
+# because we will need this column for counting how many days in a year 
+# fulfill the criteria that 
 # (1) the avg temp of the day was above 25C 
 # (2) the highest temp of the day was above 30C
 df['Year'] = df['YYYYMMDD'].dt.year
 
 
 
-#filtering out the days that fulfill the two criteria
+# filtering out the days that fulfill the two criteria
 dff = df.loc[(df["TX"] > 300) | (df["TG"] > 250)]
 print(dff)
 
-#count how many days each year(from 2000 to 2022 today) fulfill the two criteria  
+# count how many days each year(from 2000 to 2022 today) fulfill the two criteria  
 dff_count = dff.groupby(["Year"])["Year"].count()
 print(dff_count)
 
